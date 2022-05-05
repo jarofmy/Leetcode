8959d3411a475112725865d9adb78834a0b40890
Jeremy Van <jeremyvan614@gmail.com>
2022-05-04 22:16:33 -0500

    buy-sell-stocks leetcode

---
+++ buySellStocks.py
@@ -0,0 +1,21 @@
+
+def maxProfit(prices):
+    left = 0
+    right = 1
+    max_profit = 0
+    
+    while right < len(prices):
+        
+        if prices[left] < prices[right]:
+            profit = prices[right] - prices[left]
+            max_profit = max(max_profit, profit)
+        else:
+            left = right
+            
+        right += 1
+            
+    return max_profit
+
+prices = [7,1,5,3,6,4]
+max_profit = maxProfit(prices)
+print(max_profit)
